f624dc7d52312b1f822e3c19b40f362109db9e49
Oleg Luganskiy <arky.happ@gmail.com>
2021-11-17 22:01:53 +0300

    feat (be: index): add Add mutation

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,6 @@
 import { GraphQLServer } from "graphql-yoga";
 
-const typeDefs =`
+const typeDefs = `
     type Todo {
         id: ID!
         done: Boolean!
@@ -10,26 +10,51 @@ const typeDefs =`
     type Query {
         getTodos: [Todo!]!
     }
-`
+    
+    type Mutation {
+        addTodo(text: String!): Todo
+        setDone(id: ID!, done: Boolean!): Todo
+    }
+`;
 
 const resolvers = {
-    Query: {
-        getTodos: () => {
-            return todos;
-        }
-    }
+  Query: {
+    getTodos: () => {
+      return todos;
+    },
+  },
+  Mutation: {
+    addTodo: (_: unknown, { text }: { text: string }) => {
+      const newTodo = {
+        id: String(todos.length + 1),
+        text,
+        done: false,
+      };
+      todos.push(newTodo);
+      return newTodo;
+    },
+    setDone: (_: unknown, { id, done }: { id: string; done: boolean }) => {
+      const todo = todos.find((todo) => todo.id === id);
+      if (!todo) {
+        throw new Error("Todo not found");
+      }
+      todo.done = done;
+      return todo;
+    },
+  },
 };
 
 let todos = [
-    {
-        id: "1",
-        text: "test",
-        done: false
-    }
+  {
+    id: "1",
+    text: "test",
+    done: false,
+  },
 ];
 
 const server = new GraphQLServer({
-    typeDefs, resolvers
-})
+  typeDefs,
+  resolvers,
+});
 
-server.start(() => console.log("Server is running on http://localhost:4000"))
+server.start(() => console.log("Server is running on http://localhost:4000"));
